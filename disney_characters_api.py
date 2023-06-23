dd7fe2425857fc6a88a1cca66f5be1b465a9b9d8
Paola Calle <paolac@seoscholars.org>
2023-06-23 20:04:57 +0000

    Made some changes to its format

--- disney_characters_api.py
+++ disney_characters_api.py
@@ -8,11 +8,11 @@ print("------------------------------------")
 print(f"\nSome films that {input_name} is in are...\n")
 
 i = 1
-for values in data['data'][1]['films']: 
-  print(f"{i}: {values}\n")
-  i += 1
+for values in data['data'][1]['films']:
+    print(f"{i}: {values}\n")
+    i += 1
 
 if len(data['data'][1]['films']) == 0: 
-  print(f"Could not find {input_name}")
+    print(f"Could not find {input_name}")
 
 print("-----------------------------------")
