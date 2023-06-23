724781346072ba46b5e1cbdaf6700d32b36add3a
Paola Calle <paolac@seoscholars.org>
2023-06-23 19:29:28 +0000

    adding my api python file

---
+++ my_api.py
@@ -0,0 +1,15 @@
+import requests
+import pprint
+import pandas as pd
+import sqlalchemy as db
+
+response = requests.get(f"https://api.github.com/users")
+characters_data = response.json()
+
+df = pd.DataFrame(characters_data)
+engine = db.create_engine('sqlite:///data_base_name.db')
+df.to_sql('table_name', con=engine, if_exists='replace', index=False)
+
+with engine.connect() as connection:
+  query_result = connection.execute(db.text("SELECT * FROM table_name;")).fetchall()
+  print(pd.DataFrame(query_result))
