57e1a92ac1f19de782d46f088b529767688e8633
Paola Calle <paolac@seoscholars.org>
2023-06-23 20:08:19 +0000

    Changing format to my_apy.py

--- my_api.py
+++ my_api.py
@@ -11,5 +11,5 @@ engine = db.create_engine('sqlite:///data_base_name.db')
 df.to_sql('table_name', con=engine, if_exists='replace', index=False)
 
 with engine.connect() as connection:
-  query_result = connection.execute(db.text("SELECT * FROM table_name;")).fetchall()
-  print(pd.DataFrame(query_result))
+    r = connection.execute(db.text("SELECT * FROM table_name;")).fetchall()
+    print(pd.DataFrame(r))
